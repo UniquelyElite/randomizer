f940f99c0b32b505d3e6626a6007931a7b47ab50
UniquelyElite <rylanmetznerm@gmail.com>
2020-11-28 17:19:51 -0600

    added some CSS

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
 function insert() {
-    console.log('lol')
     let input = document.getElementById('itemInput').value;
     if (input != ''){
-        let items = `<p class='item'>${input}</p>`;
-        document.getElementById('items').innerHTML += items;
+        document.getElementById('items').innerHTML += `<p class='item'>${input}</p>`;
         document.getElementById('itemInput').value = '';
+        if (window.innerHeight < document.getElementById('userInput').offsetHeight) {
+            document.getElementsByTagName('body')[0].style.height = '100%'
+        }
     }
 }
 addEventListener('keydown', (event) => {
