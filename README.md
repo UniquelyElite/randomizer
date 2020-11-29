413ff702f74e5fafa6f8dd3132f6b99513ec1a81
UniquelyElite <rylanmetznerm@gmail.com>
2020-11-29 12:11:45 -0600

    items are now deleted on click

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# randomizer
-Insert items and it chooses randomly between them
+# Randomizer
+<!--
+## Live site
+[Live Site Here](https://uniquelyelite.github.io/)
+-->
+
+## Info
+
+Randomizes the given set of items, work in progress.
+
+## Contributors / Big Helpers XD
+
+[Alpox](https://github.com/alpox)
--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+let items = [];
+let itemNumber = 0;
 function insert() {
     let input = document.getElementById('itemInput').value;
     if (input != ''){
-        document.getElementById('items').innerHTML += `<p class='item'>${input}</p>`;
+        let p = document.createElement('p');
+        p.setAttribute("id", itemNumber);
+        p.classList.add('item')
+        let t = document.createTextNode(input)
+        p.appendChild(t)
+        document.getElementById('items').appendChild(p)
+        items.push(itemNumber)
         document.getElementById('itemInput').value = '';
         if (window.innerHeight < document.getElementById('userInput').offsetHeight) {
-            document.getElementsByTagName('body')[0].style.height = '100%'
+            document.getElementsByTagName('body')[0].style.height = '100%';
         }
+        document.getElementById(itemNumber).addEventListener('click', (event) => {
+            console.log(`fired@${event.target.id}`);
+            remove(event.target.id)
+            //remove(event.target.id);
+        })
+        itemNumber++;
+    }
+}
+function random() {
+    let number = Math.floor(Math.random() * items.length);
+    document.getElementById('output').innerText = `'${items[number]}' was chosen, press ENTER to return and SPACE to choose again.`;
+    document.getElementById('outputHold').style.display = 'inline';
+}
+function remove(id) {
+    items.splice(items.indexOf(id));
+    let item = document.getElementById(id);
+    if (item.parentNode) {
+        item.parentNode.removeChild(node);
     }
 }
 addEventListener('keydown', (event) => {
-    console.log(event.key)
+    console.log(event.key);
     if (event.key == 'Enter'){
         insert();
     }
